bd975b635e30205702b6aa6c9d6b0ea69d990b61
Kevawn Warren <kevawnw@gmail.com>
2022-02-22 20:18:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let random = Math.round(Math.random() * 10) +1;
 
-// Write your code here
+let max = Math.max(5, 10, 15, 20)
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2
+
+const num3 = 22
+const num4 = 6
+let mod = num3 % num4
